d4c934f32caa48225cc8ed2ce44ad32bdc9bc464
Bode Falade <bode.falade@outlook.com>
2019-02-03 17:28:55 -0600

    some styling done to the table.

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -5,7 +5,8 @@ import AdminTable from './AdminTable';
 const Admin = props => (
     <div>
         <div className="admin-page">
-        <header>Admin</header>
+        <p className="admin-page">Admin Page</p>
+        < header className ="App-header App-intro App-title App"> Admin </header>
         <AdminInput />
         <AdminTable />
         </div>
